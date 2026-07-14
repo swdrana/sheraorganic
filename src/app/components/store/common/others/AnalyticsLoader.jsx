0ec1a76ae73021d7fc36d12351aa72f22bf33226
swdRana <codingbengal@gmail.com>
2026-07-14 20:51:41 +0800

    fix: completely disable and remove Facebook Pixel script loading and tracking

--- src/app/components/store/common/others/AnalyticsLoader.jsx
+++ src/app/components/store/common/others/AnalyticsLoader.jsx
@@ -18,19 +18,6 @@ export default function AnalyticsLoader() {
           j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
           f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-MMLFB63S');
-
-        // Load Facebook Pixel
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        
-        window.fbq('init', '1813350285799787');
-        window.fbq('track', 'PageView');
       } catch (e) {
         console.error("Error loading analytics:", e);
       }
